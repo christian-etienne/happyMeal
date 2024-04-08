3b5246678749ddf2b6c4b20958387527c2c067e0
Rémy R <remy.rabischung@laplateforme.io>
2024-04-08 15:14:03 +0200

    Mise en place json et nouvelle fonction

--- js/random-recipe.js
+++ js/random-recipe.js
@@ -1,29 +1,19 @@
-// Liste des recettes
-const recipes = [
-    { name: "Poulet rôti aux herbes", duration: "60 mins", image: "images/poulet-roti.jpg" },
-    { name: "Ratatouille provençale", duration: "60 mins", image: "images/ratatouille.jpg" },
-    { name: "Risotto aux champignons", duration: "40 mins", image: "images/risotto.webp" },
-    { name: "Salade de quinoa aux légumes grillés", duration: "30 mins", image: "images/salade-quinoa.jpeg" },
-    { name: "Tarte aux pommes", duration: "60 mins", image: "images/tarte-pomme.jpg" },
-    { name: "Soupe de lentilles", duration: "60 mins", image: "images/soupe-lentille.jpg" },
-    { name: "Pâtes Carbonara", duration: "20 mins", image: "images/pate-carbonara.jpg" },
-    { name: "Salade de fruits frais", duration: "20 mins", image: "images/salade-fruit.jpg" },
-    { name: "Salade César", duration: "20 mins", image: "images/salade-cesar.jpg" },
-    { name: "Muffins aux myrtilles", duration: "30 mins", image: "images/muffin.webp" },
-    { name: "Lasagnes végétariennes", duration: "90 mins", image: "images/lasagne-vege.png" },
-    { name: "Tiramisu", duration: "30 mins", image: "images/tiramisu.jpg" }
-
-    // Ajoutez d'autres recettes ici
-];
-
-// Nombre de recettes à afficher
-const numberOfRecipesToShow = 3;
+// Fonction pour charger les recettes depuis le fichier JSON
+function loadRecipesFromJSON() {
+    fetch('data.json') // Modifier le chemin pour pointer vers data.json à la racine du dossier
+        .then(response => response.json())
+        .then(data => {
+            displayRandomRecipes(data.recettes);
+        })
+        .catch(error => console.error('Erreur lors du chargement des recettes :', error));
+}
 
 // Fonction pour afficher une sélection aléatoire de recettes
-function displayRandomRecipes() {
+function displayRandomRecipes(recipes) {
     const recipeContainer = document.getElementById("recipe-container");
     recipeContainer.innerHTML = '';
 
+    const numberOfRecipesToShow = 3;
     const randomIndex = Math.floor(Math.random() * (recipes.length - numberOfRecipesToShow + 1));
     const randomRecipes = recipes.slice(randomIndex, randomIndex + numberOfRecipesToShow);
 
@@ -32,15 +22,15 @@ function displayRandomRecipes() {
         col.classList.add('col');
 
         col.innerHTML = `
-            <div class="card h-100 shadow-sm"> <!-- Ajoutez la classe h-100 pour définir une hauteur fixe pour la carte -->
-                <img src="${recipe.image}" class="card-img-top" alt="Image de la recette"> <!-- Ajoutez la classe card-img-top pour contrôler le dimensionnement de l'image -->
+            <div class="card h-100 shadow-sm">
+                <img src="${recipe.image}" class="card-img-top" alt="${recipe.nom}">
                 <div class="card-body">
-                    <p class="card-text">${recipe.name}</p>
+                    <p class="card-text">${recipe.nom}</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Voir la recette</button>
+                            <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#${recipe.nom.replaceAll(' ', '-')}">Voir la recette</button>
                         </div>
-                        <small class="text-body-secondary">${recipe.duration}</small>
+                        <small class="text-body-secondary">${recipe.temps_preparation}</small>
                     </div>
                 </div>
             </div>
@@ -50,5 +40,5 @@ function displayRandomRecipes() {
     });
 }
 
-// Appeler la fonction pour afficher les recettes aléatoires au chargement de la page
-displayRandomRecipes();
+// Appeler la fonction pour charger les recettes et les afficher au chargement de la page
+loadRecipesFromJSON();
