5d1b44e83a25de41ee5804729c6545da67a81a0a
Rémy R <remy.rabischung@laplateforme.io>
2024-04-10 15:21:22 +0200

    fonction et page liste OK

--- js/liste.js
+++ js/liste.js
@@ -1,9 +1,15 @@
 // Fonction pour ajouter un ingrédient à la liste de courses
 function addToShoppingList(ingredient) {
   const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
-  shoppingList.push(ingredient);
-  localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-  displayShoppingList();
+
+  // Vérifie si l'ingrédient n'est pas déjà présent dans la liste de courses
+  if (!shoppingList.includes(ingredient)) {
+      shoppingList.push(ingredient);
+      localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+      displayShoppingList();
+  } else {
+      console.log(`L'ingrédient "${ingredient}" est déjà dans la liste de courses.`);
+  }
 }
 
 // Fonction pour afficher la liste de courses
@@ -16,13 +22,24 @@ function displayShoppingList() {
   shoppingList.forEach(ingredient => {
       const listItem = document.createElement('li');
       listItem.textContent = ingredient;
+
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Supprimer';
+      removeButton.classList.add('btn', 'btn-warning', 'btn-sm', 'ms-2');
+      removeButton.addEventListener('click', function() {
+          removeFromShoppingList(ingredient);
+      });
+
+      listItem.appendChild(removeButton);
       shoppingListContainer.appendChild(listItem);
   });
 }
 
-// Fonction pour effacer la liste de courses
-function clearShoppingList() {
-  localStorage.removeItem('shoppingList');
+// Fonction pour supprimer un ingrédient de la liste de courses
+function removeFromShoppingList(ingredient) {
+  let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+  shoppingList = shoppingList.filter(item => item !== ingredient);
+  localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
   displayShoppingList();
 }
 
